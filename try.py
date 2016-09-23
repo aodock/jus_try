7fe7e37f8e4c273aa6d1eb6916bd0955541c8d4a
aodock <aodock@gmail.com>
2016-09-23 08:37:35 +0530

    commit after 1st branch

--- try.py
+++ try.py
@@ -1,4 +1,5 @@
 try:
-    raise
-except:
-    print 'exception1'
+    n = int(raw_input("enter a number : "))
+    print 'Entered value : ', n
+except ValueError:
+    print 'u\'v entered invalid number,\nmake sure its an int type'
