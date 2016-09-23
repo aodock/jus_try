a9e1daf566bf2a9b9c79c6c8f36367b4f88bdf19
aodock <aodock@gmail.com>
2016-09-23 14:11:19 +0530

    added while to try

--- try.py
+++ try.py
@@ -1,5 +1,7 @@
 try:
-    n = int(raw_input("enter a number : "))
-    print 'Entered value : ', n
+    while True:
+        n = int(raw_input("enter a number : "))
+        print 'Entered value : ', n
+        break
 except ValueError:
     print 'u\'v entered invalid number,\nmake sure its an int type'
