5d01a48b57f855a89a7ebedd384ae85e12300900
aodock <aodock@gmail.com>
2016-09-24 13:56:02 +0530

    added I/O Exceptions and multiple exceptions

--- try.py
+++ try.py
@@ -1,5 +1,18 @@
+import sys
+
 try:
-    n = int(raw_input("enter a number : "))
-    print 'Entered value : ', n
+    f = open('integers.txt')
+    s = f.readline()
+    i = int(s.strip())
+except IOError as e:
+    errno, strerror = e.args
+    print "I/O error({0}): {1}".format(errno, strerror)
+    print " e0 = ", e[0]
+    print " e1 = ", e[1]
+    for i in e:
+        print i
 except ValueError:
     print 'u\'v entered invalid number,\nmake sure its an int type'
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
